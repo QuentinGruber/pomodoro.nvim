b8985965d786da9bad14f43ccd774eef129b6826
quentingruber <quentingruber@gmail.com>
2024-09-27 18:26:32 +0200

    use opts value directly

--- lua/pomodoro/tests/pomodoro_spec.lua
+++ lua/pomodoro/tests/pomodoro_spec.lua
@@ -13,16 +13,30 @@ describe("useless tests", function()
         opts.breaks_before_long = 4
         opts.start_at_launch = false
         pomodoro.setup(opts)
-        -- TODO: use the opts value directly
-        assert(pomodoro.work_duration == 10 * MIN_IN_MS, "Opt work_duration")
-        assert(pomodoro.break_duration == 10 * MIN_IN_MS, "Opt break_duration")
-        assert(pomodoro.breaks_before_long == 4, "Opt breaks_before_long")
         assert(
-            pomodoro.long_break_duration == 15 * MIN_IN_MS,
+            pomodoro.work_duration == opts.work_duration * MIN_IN_MS,
+            "Opt work_duration"
+        )
+        assert(
+            pomodoro.break_duration == opts.break_duration * MIN_IN_MS,
+            "Opt break_duration"
+        )
+        assert(
+            pomodoro.breaks_before_long == opts.breaks_before_long,
+            "Opt breaks_before_long"
+        )
+        assert(
+            pomodoro.long_break_duration == opts.long_break_duration * MIN_IN_MS,
             "Opt long_break_duration"
         )
-        assert(pomodoro.delay_duration == 10 * MIN_IN_MS, "Opt delay_duration")
-        assert(pomodoro.start_at_launch == false, "Opt start_at_launch")
+        assert(
+            pomodoro.delay_duration == opts.delay_duration * MIN_IN_MS,
+            "Opt delay_duration"
+        )
+        assert(
+            pomodoro.start_at_launch == opts.start_at_launch,
+            "Opt start_at_launch"
+        )
     end)
     --TODO: tests
 end)
